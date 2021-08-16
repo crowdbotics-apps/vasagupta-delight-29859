539bfb277ffbcf3fb796a3aa5a0ef468a14a85e9
Crowdbotics <team@crowdbotics.com>
2021-08-16 21:15:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "vasagupta_delight_29859Identifier",
+  url: "https://vasagupta_delight_29859Identifier.botics.co",
+  api: "https://vasagupta_delight_29859Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
